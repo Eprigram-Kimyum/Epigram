2f93a95ac5dd72d64ee63b14efcd7f7f10db821c
김윰 <kimyumin71@gmail.com>
2026-06-17 03:27:25 +0900

    feat: image API 작업

---
+++ src/apis/image/image.ts
@@ -0,0 +1,14 @@
+import instance from '@/apis/instance';
+import { UploadImageParams, UploadImageResponse } from './type';
+
+export const uploadImage = async ({
+  teamId,
+  image,
+}: UploadImageParams): Promise<UploadImageResponse> => {
+  const formData = new FormData();
+  formData.append('image', image);
+
+  const response = await instance.post<UploadImageResponse>(`/${teamId}/images/upload`, formData);
+
+  return response.data;
+};
